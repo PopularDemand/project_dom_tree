e84f8b2f02ee6d709617363cf0dc66415a8c06ba
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-10 12:49:59 -0700

    Finish tag parser!

--- tag_parser.rb
+++ tag_parser.rb
@@ -1,36 +1,29 @@
 Node = Struct.new(:type, :classes, :id, :name)
 TYPE_RX = /<(.+?) /
 ATTR_RX = /([a-z_\-]+)=("|')([^'"]+)("|')/
+
 def tag_parser(str)
 
-
-  type = str.match(TYPE_RX)
+  type = str.match(TYPE_RX)[1]
   attrs = str.scan(ATTR_RX)
-  puts "attrs: #{attrs}"
-  attr_hash = Hash.new( [] )
+  attr_hash = Hash.new()
 
   attrs.each do |attribute|
-    attr_hash[attribute[0]] << attribute[2]
+    attr_hash[attribute[0]] = attribute[2]
   end
 
-  puts "att hash: #{attr_hash}"
+  attr_hash["class"] = attr_hash["class"].split(" ") if attr_hash["class"]
 
   Node.new(type, attr_hash["class"], attr_hash["id"], attr_hash["name"])
 end
 
-# "<p class='foo bar' id='baz' name='fozzie'>"
-
-# "<div id = 'bim'>"
-
-# # "<img src='http://www.example.com' title='funny things'>"
-# type regex: /<(.+?) /
-# attr regex: /[a-z_\-]+)=("|')([^'"]+)("|')/ (scan)
-# [["src", "'", "http://www.example.com", "'"],
-#  ["title", "'", "funny things", "'"]]
-
-# <div class='foo bar' id='main' name=''>
-
-t = tag_parser('<div class="foo bar" id="main" name="">')
-# puts  t.type
+# t = tag_parser('<div class="foo bar" id="main" name="">')
+# puts t.type
 # p t.classes
 # p t.id
+# p t.name
+
+# v = tag_parser("<div id = 'bim'")
+# puts v.type
+# p v.classes
+# p v.id
