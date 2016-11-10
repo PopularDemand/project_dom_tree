939b82d2c5a952c643e47129ea6ef7499b3e17b0
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-10 14:56:13 -0700

    Fix type regex to account for attr-less html

--- tag_parser.rb
+++ tag_parser.rb
@@ -1,5 +1,5 @@
 Node = Struct.new(:type, :classes, :id, :name)
-TYPE_RX = /<(.+?) /
+TYPE_RX = /<(.+?)( |>)/
 ATTR_RX = /([a-z_\-]+)=("|')([^'"]+)("|')/
 
 def tag_parser(str)
@@ -17,13 +17,13 @@ def tag_parser(str)
   Node.new(type, attr_hash["class"], attr_hash["id"], attr_hash["name"])
 end
 
-# t = tag_parser('<div class="foo bar" id="main" name="">')
-# puts t.type
-# p t.classes
-# p t.id
-# p t.name
+t = tag_parser('<div class="foo bar" id="main" name="">')
+puts t.type
+p t.classes
+p t.id
+p t.name
 
-# v = tag_parser("<div id = 'bim'")
-# puts v.type
-# p v.classes
-# p v.id
+v = tag_parser("<div id = 'bim'>")
+puts v.type
+p v.classes
+p v.id
